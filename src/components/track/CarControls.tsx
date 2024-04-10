57eeccb98f282c14cc80c1c199dad52f5216128d
solidados <geekbrains.pk@internet.ru>
2024-04-10 22:05:19 +0400

    feat: create car track, it's controls and styles

---
+++ src/components/track/CarControls.tsx
@@ -0,0 +1,10 @@
+import Button from '../../ui/buttons/Button.tsx';
+
+const CarControls = () => (
+  <div className="car-controls">
+    <Button className={'car-controls-start'} text={'a'} />
+    <Button className={'car-controls-stop'} text={'b'} />
+  </div>
+);
+
+export default CarControls;
