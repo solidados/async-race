1a11257675ac7c6f26cf3809d37454ba102b4d86
solidados <geekbrains.pk@internet.ru>
2024-04-18 16:07:31 +0400

    refactor: update Header and Button components upon new PageContext

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,28 @@
+import usePageContext from '../../modules/helpers/hooks/usePageContext.tsx';
 import Button from '../../ui/buttons/Button.tsx';
 import './styles.scss';
 
-const Header = () => (
-  <header className="header">
-    <div className="header-container">
-      <nav className="nav">
-        <Button className={'nav-button'} text={'Garage'} />
-        <Button className={'nav-button'} text={'Winners'} />
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const { togglePage } = usePageContext();
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <nav className="nav">
+          <Button
+            className={'nav-button'}
+            onClick={togglePage}
+            text={'Garage'}
+          />
+          <Button
+            className={'nav-button'}
+            onClick={togglePage}
+            text={'Winners'}
+          />
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/ui/buttons/Button.tsx
+++ src/ui/buttons/Button.tsx
@@ -1,16 +1,16 @@
-import { FC } from 'react';
+import { ButtonHTMLAttributes, FC } from 'react';
 
 import './styles.scss';
 
-type ButtonProps = {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   className: string;
-  // onClick: () => void;
   text: string;
 };
 
 const Button: FC<ButtonProps> = (props) => (
-  // <button onClick={props.onClick}>{props.text}</button>
-  <button className={props.className}>{props.text}</button>
+  <button className={props.className} onClick={props.onClick}>
+    {props.text}
+  </button>
 );
 
 export default Button;
