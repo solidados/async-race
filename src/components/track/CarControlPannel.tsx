0e7ccc6ebec33111f54cef4af283bfad662e034a
solidados <geekbrains.pk@internet.ru>
2024-04-18 12:19:07 +0400

    refactor: add wrapper for car controls and styled

--- src/components/track/CarControlPannel.tsx
+++ src/components/track/CarControlPannel.tsx
@@ -2,9 +2,12 @@ import CarControls from './CarControls.tsx';
 import CarTrackControls from './CarTrackControls.tsx';
 
 const CarControlPannel = () => (
-  <div className="car-control-pannel">
-    <CarTrackControls />
-    <CarControls />
+  <div className="car-control-wrapper">
+    <p>Mercedes GQL-500</p>
+    <div className="car-control-pannel">
+      <CarTrackControls />
+      <CarControls />
+    </div>
   </div>
 );
 
